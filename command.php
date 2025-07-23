8ae38161786282335236e866152bae38acee69a8
Md.Mazharul Islam <41231207+mazharul26@users.noreply.github.com>
2025-07-23 18:45:05 +0600

    Update command.php

--- command.php
+++ command.php
@@ -1,1 +1,9 @@
+1. laravel Project ip dia serve korar code
+php artisan serve --host=0.0.0.0 --port=8000
+
+2. react js project IP dia run corar cammand
+
 npm run dev -- --host=0.0.0.0 --port=5174
+
+3. php artisan command
+php -S localhost:8000 -t .
